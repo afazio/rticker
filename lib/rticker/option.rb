8cfa1de824522049432402c6cc4ce626437c66b4
Alfred J. Fazio <alfred.fazio@gmail.com>
2011-08-12 16:25:15 -0400

    Fixed bug in options fetcher: closures + threads = smashing variables

--- lib/rticker/option.rb
+++ lib/rticker/option.rb
@@ -23,34 +23,42 @@ module RTicker
     end
 
     def Option.update (entries)
+      go = Proc.new do |entry|
+        Thread.new { Option.run_update entry }
+      end
+
       threads = []
       for entry in entries
         # Can only grab one option contract at a time, so request each one in
         # a separate thread.
-        threads << Thread.new do
-          uri = "http://finance.yahoo.com/q?s=%s" % CGI::escape(entry.symbol)
-          response = Net::HTTP.get(URI.parse uri) rescue Thread.current.exit
-          # Rake through the HTML and find the bits we want.
-          # This can be a bit messy.
-          begin
-            bid = /id="yfs_b00_[^"]*">([^<]*)</.match(response)[1]
-            ask = /id="yfs_a00_[^"]*">([^<]*)</.match(response)[1]
-          rescue Exception => e
-            # These results aren't available from about 9am to 9:30am.
-            # Yahoo's results are often 20-30 minutes behind.
-            Thread.current.exit
-          end
-          if (bid.to_f != entry.bid or ask.to_f != entry.ask) and not entry.bid.nil?
-            # The price has changed
-            entry.last_changed = Time.now()
-          end
-          entry.bid = bid.to_f
-          entry.ask = ask.to_f
-          # The value is the mean average of the bid/ask spread
-          entry.curr_value = (entry.bid + entry.ask) / 2
-        end
+        threads << go.call(entry)
       end
       threads.each {|t| t.join}
+    end
+
+    private
+    
+    def Option.run_update (entry)
+      uri = "http://finance.yahoo.com/q?s=%s" % CGI::escape(entry.symbol)
+      response = Net::HTTP.get(URI.parse uri) rescue Thread.current.exit
+      # Rake through the HTML and find the bits we want.
+      # This can be a bit messy.
+      begin
+        bid = /id="yfs_b00_[^"]*">([^<]*)</.match(response)[1]
+        ask = /id="yfs_a00_[^"]*">([^<]*)</.match(response)[1]
+      rescue Exception => e
+        # These results aren't available from about 9am to 9:30am.
+        # Yahoo's results are often 20-30 minutes behind.
+        Thread.current.exit
+      end
+      if (bid.to_f != entry.bid or ask.to_f != entry.ask) and not entry.bid.nil?
+        # The price has changed
+        entry.last_changed = Time.now()
+      end
+      entry.bid = bid.to_f
+      entry.ask = ask.to_f
+      # The value is the mean average of the bid/ask spread
+      entry.curr_value = (entry.bid + entry.ask) / 2
     end
   end
 
